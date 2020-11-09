cd27a4372caf6a6c65d9e635db3a2aa22b1e903f
christian <ccastro@gmx.us>
2020-11-09 17:10:33 -0300

    up

--- shiny_casen/CASEN_app.R
+++ shiny_casen/CASEN_app.R
@@ -137,8 +137,7 @@ server <- function(input, output, session) {
         
         tabPanel("Despliegue de la tabla",
                  fluidRow(column(3, includeMarkdown("info_2006_tabla.md")),
-                          column(12,  tableOutput("contents12"),
-                                 dataTableOutput('table')))),
+                          column(12,  dataTableOutput('table')))),
         
         tabPanel("Frecuencias por preguntas", fluidRow(
           column(12, includeMarkdown("info_2006_frec.md")),
@@ -196,9 +195,12 @@ server <- function(input, output, session) {
         tabPanel("Introducción",
                  fluidRow(column(9, includeMarkdown("about_intro.md")))),
         
+
+        
+        
         tabPanel("Despliegue de la tabla",
-                 fluidRow(column(9, includeMarkdown("about_pobporin_vei.txt")),
-                          column(3,  tableOutput("contents12")))),
+                 fluidRow(column(3, includeMarkdown("about_pobporin_vei.txt")),
+                          column(12,dataTableOutput("table2009ymt")))),
         
         tabPanel("Frecuencias por preguntas", fluidRow(
           column(12, includeMarkdown("about_educacion.md.txt")),
